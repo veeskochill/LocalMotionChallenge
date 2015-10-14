aa20b7bd4af282818ba04803ce52eb156b0cde54
veeskochill <alex.vyskocil@gmail.com>
2015-10-14 00:58:52 -0400

    pick up all the people travelling in the same direction

--- localmotion.js
+++ localmotion.js
@@ -1,18 +1,7 @@
-function showProps(obj, objName) {
-  var result = "";
-  for (var i in obj) {
-    if (obj.hasOwnProperty(i)) {
-        result += objName + "." + i + " = " + obj[i] + "\n";
-    }
-  }
-  return result;
-}
-
 //vehicles :name, x, y, peoples, picks, moveUp, moveDown, moveLeft, moveRight, moveTo, pick
 //peoples :name, x, y, origin, destination, time, time0, img
 //buildings :name, x, y
-var flag = false
-var dest  = 2
+
 var convert = 'ABCDEFG'
 
 var start_flag = true
@@ -21,10 +10,6 @@ var dests = [0,1,2,3,4]
 
 
 function turn(vehicles,peoples,buildings){
-   //documentation can be found in the source
-   //Good luck :)
-    //console.log(Object.getOwnPropertyNames(buildings[0]))
-    //init to some dest
     for(var vid in vehicles)
     {
         vehicles[vid].moveTo(buildings[dests[vid]])
@@ -35,41 +20,24 @@ function turn(vehicles,peoples,buildings){
     for(var vid in vehicles)
     {
         if(vehicles[vid].x == buildings[dests[vid]].x && vehicles[vid].y == buildings[dests[vid]].y)
+            //pickup all people travelling in the same direction
+            var new_dest = -1
             for(var pid in peoples)
             {
                 if(peoples[pid].origin == buildings[dests[vid]].name)
                 {
-                    vehicles[vid].pick(peoples[pid])
-                    dests[vid] = convert.indexOf(peoples[pid].destination)
-                    break
+                    if(new_dest == -1)
+                    {
+                      new_dest = peoples[pid].destination
+                      dests[vid] = convert.indexOf(new_dest)
+
+                    }
+                    if(new_dest == peoples[pid].destination)
+                    {
+                      vehicles[vid].pick(peoples[pid])
+                    }
+  //                  break
                 }
             }
     }
-    
-    
-    /*
-    if(vehicles[4].x == buildings[dest].x && vehicles[4].y == buildings[dest].y)
-    {
-        flag = true
-        for(var p in peoples)
-        {
-            if(peoples[p].origin == buildings[dest].name)
-            {
-                vehicles[4].pick(peoples[p])
-                dest = convert.indexOf(peoples[p].destination)
-                console.log(dest)
-                //console.log(vehicles[4].peoples)
-              //  vehicles[4].moveTo(peoples[p].destination)
-                break
-                //console.log(peoples[p].origin, peoples[p].destination)
-            }
-        }
-    }
- //   if(flag == true)
-   // {
-//        vehicles[4].moveTo(buildings[1])
-  //  }
-   // console.log(peoples[4].time, peoples[4].time0);
-    }
-    */
 }
